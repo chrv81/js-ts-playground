0a2e587b3f4936ddfbbf99b0593d2d14d168783e
chrv81 <vochris81@gmail.com>
2025-07-17 21:57:36 -0400

    Add download button functionality and enhance settings popup messaging

--- playground.js
+++ playground.js
@@ -84,6 +84,18 @@ window.require(['vs/editor/editor.main'], async (monaco) => {
     tabSize: 2,
     insertSpaces: true,
     autoIndent: 'advanced',
+    suggestOnTriggerCharacters: true,
+    quickSuggestions: true,
+    parameterHints: { enabled: true },
+    folding: true,
+    foldingStrategy: 'auto',
+    bracketPairColorization: { enabled: true },
+    find: {
+      addExtraSpaceOnTop: true,
+      seedSearchStringFromSelection: true,
+    },
+    hover: { enabled: true },
+    contextmenu: true,
   });
 });
 
@@ -149,13 +161,28 @@ window.addEventListener('change', (e) => {
 /**
  * Quick Pop-up for dev debug
  */
-const showSettingsPopup = (message) => {
+const MessageType = {
+  SUCCESS: 'positive',
+  INFO: 'blue',
+  WARNING: 'negative',
+  DARK: 'black',
+  CAUTION: 'orange',
+};
+
+const showSettingsPopup = (message, sec = 5000, messageType) => {
   const popup = document.getElementById('settings-popup');
+
+  // Remove previous type classes
+  popup.classList.remove('positive', 'blue', 'negative', 'black', 'orange');
+  // Add the new type class
+  popup.classList.add(messageType);
+
   popup.innerHTML = `<i class="info icon"></i> ${message}`;
   popup.style.display = 'block';
+
   setTimeout(() => {
     popup.style.display = 'none';
-  }, 5000);
+  }, sec);
 };
 
 /**
@@ -258,5 +285,9 @@ const compilerLanguage = {
  * This function is called when the "Download Code" button is clicked
  */
 const downloadCode = () => {
-  alert('Download Code function called');
+  showSettingsPopup(
+    'Sorry, download feature is not implemented yet.',
+    2500,
+    MessageType.WARNING
+  );
 };
